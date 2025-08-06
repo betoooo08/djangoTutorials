786c6683896530e513ad71880f50d1d3d4f4f0d2
betoooo08 <alcefo@gmail.com>
2025-08-06 00:57:56 -0500

    Agregado contacto: vista, ruta y template de contact

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, aboutPageView, contactPageView
 
 urlpatterns =[
-    path("", homePageView, name='home')
+    path('', homePageView.as_view(), name='home'),
+    path('about/', aboutPageView.as_view(), name='about'),
+    path('contact/', contactPageView.as_view(), name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-from django.http import HttpResponse
-# Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello World!')
+class homePageView(TemplateView):
+    template_name = "pages/home.html"
+    
+class aboutPageView(TemplateView):
+ template_name = "pages/about.html"
+
+ def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context.update({
+        "title": "About us - Online Store",
+        "subtitle": "About us",
+        "description": "This is an about page ...",
+        "author": "Developed by: Alberto Daniel Cervantes Forero",
+    })
+    return context
+
+class contactPageView(TemplateView):
+    template_name = "pages/contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Contacto"
+        context["subtitle"] = "Contáctanos"
+        context["email"] = "fake-email@example.com"
+        context["address"] = "123 Fake Street, Medellín, Colombia"
+        context["phone"] = "+57 301 123 4567"
+        return context
